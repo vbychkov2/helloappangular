e3f0716df9b9a5e2318d67cc68a85ffa09833f48
vbychkov2 <vbychkov@solit-clouds.ru>
2025-02-23 21:30:08 +0300

    add module and off standalone

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,8 @@
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 
 @Component({
-  // определяет селектор css для HTML-элемента
   selector: 'my-app',
-  // компонент будет независимым
-  standalone: true,
-  // импортирует в компонент другие модули
-  imports: [FormsModule],
+  standalone: false,
   template: `<label>Введите имя:</label>
     <input [(ngModel)]="name" placeholder="name" />
     <h1>Добро пожаловать {{ name }}!</h1>`,
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,4 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { AppComponent } from './app/app.component';
-
-// При ошибках -> выводятся на консоль
-bootstrapApplication(AppComponent).catch((e) => console.error(e));
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppModule } from './app/app.module';
+const platform = platformBrowserDynamic();
+platform.bootstrapModule(AppModule);
